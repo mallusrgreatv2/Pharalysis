9b1a223d5c0d342332a677c8ba88c2e0a5cd65ed
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2021-05-30 19:09:36 +0530

    Update suggestions.py

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -27,39 +27,21 @@ class Suggestions(Cog):
             await ctx.send(e)
 
     @commands.command()
-    async def suggest(self, ctx):
+    async def suggest(self, ctx, *, suggestion):
         data = read_json("suggestion")
         data = data[f"{ctx.guild.id}"]
         if not data:
             await ctx.send("No suggestion channel found. `{p}setSuggestionChannel #channel`")
             return
-        channel: discord.TextChannel = self.bot.get_channel(data)
-
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-        embed = discord.Embed(title="Suggestion",
-                              description="Share your suggestion below!")
-        embed.set_footer(text="Type 'cancel' to cancel.")
-        sent = await ctx.send(embed=embed)
-        try:
-            response = await self.bot.wait_for('message', check=check, timeout=15)
-        except asyncio.TimeoutError:
-            await ctx.send("Suggestion timed out.")
         
-        if response.content.lower() == 'cancel':
-            await ctx.send("Cancelled the suggestion.", delete_after=6)
-            await response.delete()
-            await sent.delete()
-            await ctx.message.delete()  
-            return
-
+        channel = await self.bot.get_channel(f"{data}")
         emb = discord.Embed(title="New Suggestion!",
-                            description=response.content)
-        emb.set_footer(text=f"Suggested by {ctx.author.name}")
+                            description=suggestion)
+        emb.set_footer(text=f"Suggested by {ctx.author.name}#{ctx.author.discriminator}")
 
-        await channel.send(embed = emb)
-        await response.delete()
-        await sent.delete()
+        msg = await channel.send(embed = emb)
+        msg.add_reaction("✅")
+        msg.add_reaction("x")
         await ctx.message.delete()
 
 
